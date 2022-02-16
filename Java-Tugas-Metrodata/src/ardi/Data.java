b3c8c927ffcb0fc568dbbea72d7f4e8f4a0d78f1
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2022-02-16 17:36:34 +0700

    Add dummy book data

---
+++ Java-Tugas-Metrodata/src/ardi/Data.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ardi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author RAI
+ */
+public class Data {
+
+    static Map<Integer, Book> getData() {
+        Map<Integer, Book> books = new HashMap<Integer, Book>()
+        {{
+            put(1, new Book("1-1322", "berani bahagia", "Ichiro Kishimi", "284", 2));
+            put(2, new Book("2-2242", "dunia cinta filosofis kahlil gibran", "Fahruddin Faiz", "274", 3));
+            put(3, new Book("3-2122", "berani tidak disukai", "Ichiro Kishimi", "284", 5));
+            put(4, new Book("4-2312", "meditations", "Marcus Aurelius", "295", 1));
+            put(5, new Book("5-2142", "sebelum filsafat", "Fahruddin Faiz", "274", 3));
+        }};
+        return books;
+    }
+}
