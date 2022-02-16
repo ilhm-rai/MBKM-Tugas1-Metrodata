8559e28563277691115bcd0e5f99f45d99f958e4
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2022-02-16 18:15:31 +0700

    random key

--- Java-Tugas-Metrodata/src/ardi/Data.java
+++ Java-Tugas-Metrodata/src/ardi/Data.java
@@ -17,9 +17,9 @@ public class Data {
     static Map<Integer, Book> getData() {
         Map<Integer, Book> books = new HashMap<Integer, Book>()
         {{
-            put(1, new Book("1-1322", "berani bahagia", "Ichiro Kishimi", "284", 2));
-            put(2, new Book("2-2242", "dunia cinta filosofis kahlil gibran", "Fahruddin Faiz", "274", 3));
-            put(3, new Book("3-2122", "berani tidak disukai", "Ichiro Kishimi", "284", 5));
+            put(3, new Book("1-1322", "berani bahagia", "Ichiro Kishimi", "284", 2));
+            put(7, new Book("2-2242", "dunia cinta filosofis kahlil gibran", "Fahruddin Faiz", "274", 3));
+            put(1, new Book("3-2122", "berani tidak disukai", "Ichiro Kishimi", "284", 5));
             put(4, new Book("4-2312", "meditations", "Marcus Aurelius", "295", 1));
             put(5, new Book("5-2142", "sebelum filsafat", "Fahruddin Faiz", "274", 3));
         }};
