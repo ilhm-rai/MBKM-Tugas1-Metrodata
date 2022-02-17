c7cd6f7a72930c7f4f6388701118eb65f3b4b3c0
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2022-02-17 17:28:10 +0700

    refactor

--- segment-satu/Tugas1/src/ardi/Data.java
+++ segment-satu/Tugas1/src/ardi/Data.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tugas1.ardi;
+package ardi;
 
 import java.util.HashMap;
 import java.util.Map;
